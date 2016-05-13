ed22b6f52ad612177f2fcf2fa7ee23321f54e552
Your Name <zhangxiaocong_2011@yeah.net>
2016-05-13 21:37:23 +0800

    这这是一个新的类

---
+++ a_内容发布者/src/main/java/com/example/michael/a_/MainActivity1.java
@@ -0,0 +1,17 @@
+package com.example.michael.a_;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.widget.Toast;
+
+/**
+ * Created by Michael on 2016/5/13.
+ */
+public class MainActivity1 extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Toast.makeText(getApplicationContext(),"fgggggg",Toast.LENGTH_LONG).show();
+    }
+}
